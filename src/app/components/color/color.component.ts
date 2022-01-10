d42510545f0f8949ae3222ad9fcc7ce6e7a539f3
aneylon <arlen.m.neylon@gmail.com>
2022-01-10 14:28:44 -0700

    Add functions from old color app.

--- src/app/components/color/color.component.ts
+++ src/app/components/color/color.component.ts
@@ -15,4 +15,22 @@ export class ColorComponent implements OnInit {
     // rbg(red,blue,green) 256
     // hex, '#000000'
   }
+  makeHexString(red:number, green:number, blue:number) {
+    return Array.prototype.reduce.call(arguments, (acc, cur) => {
+      let tmp = cur.toString(16)
+
+      if(tmp.length == 1) tmp = '0' + tmp
+
+      return acc + tmp
+    }, '#')
+  }
+
+  makeRGBString(red: number, green:number , blue: number){
+    return `rgb(${red}, ${green}, ${blue})`
+  }
+
+  randomColorNumber(max:number){
+    max = max || 255
+    return Math.floor((Math.random() * max) + 1)
+  }
 }
